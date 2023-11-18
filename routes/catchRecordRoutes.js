3b91260e37c59a68c817493767520b0a93fa26b0
Rhys Smoker <r.smoker@gmail.com>
2023-11-18 13:37:31 -0800

    Enhance Data Fetching and Display Functionality
    Backend:
    - Modified '/catchrecords' endpoint in 'catchRecordRoutes.js' to support dynamic fetching of catch records.
    - Added 'limit' and 'skip' query parameters to enable controlled data retrieval and support pagination.
    
    Frontend:
    - Updated 'CatchDataComponent.js' to fetch and display multiple records from the backend.
    - Implemented useState and useEffect hooks for data fetching and state management.
    - Adjusted the data handling to accommodate the new backend response format.

--- routes/catchRecordRoutes.js
+++ routes/catchRecordRoutes.js
@@ -9,12 +9,15 @@ console.log(
 
 router.get("/catchrecords", async (req, res) => {
 	try {
-		// Fetch the first complete catch record
-		const record = await CatchRecord.findOne({});
-		if (!record) {
+		const limit = parseInt(req.query.limit) || 10; // Default to 10 records if limit is not specified
+		const skip = parseInt(req.query.skip) || 0; // Default to start from the beginning if skip is not specified
+
+		// Fetch the catch records with limit and skip
+		const records = await CatchRecord.find({}).limit(limit).skip(skip);
+		if (!records || records.length === 0) {
 			return res.status(404).send("No catch records found");
 		}
-		res.json([record]); // Send the record within an array
+		res.json(records);
 	} catch (error) {
 		console.error("Error in catchrecords route:", error);
 		res.status(500).send("Internal Server Error");
--- src/Components/CatchDataComponent.js
+++ src/Components/CatchDataComponent.js
@@ -1,36 +1,91 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import {
+	createColumnHelper,
+	flexRender,
+	getCoreRowModel,
+	useReactTable,
+} from "@tanstack/react-table";
 
 function CatchRecords() {
-	const [records, setRecords] = useState([]);
+	const [data, setData] = useState([]);
 
+	// Define columns inside the component to avoid ESLint issues
+	const columnHelper = createColumnHelper();
+	const columns = [
+		columnHelper.accessor("weekEndDate", {
+			header: "Week End Date",
+			cell: (info) => info.getValue(),
+		}),
+		columnHelper.accessor("vessel", {
+			header: "Vessel",
+			cell: (info) => info.getValue(),
+		}),
+		columnHelper.accessor("area", {
+			header: "Area",
+			cell: (info) => info.getValue(),
+		}),
+		columnHelper.accessor("halibut", {
+			header: "Halibut",
+			cell: (info) => info.getValue(),
+		}),
+	];
+
+	// Fetch data from the API
 	useEffect(() => {
-		async function fetchData() {
+		console.log("Component mounted. Starting to fetch data.");
+
+		const fetchData = async () => {
 			try {
+				console.log("Sending request to the API...");
 				const response = await fetch("http://localhost:4000/api/catchrecords");
-				if (!response.ok) {
-					throw new Error("Network response was not ok");
-				}
-				const data = await response.json();
-				setRecords(data);
+				const json = await response.json();
+				console.log("Data received from API:", json.slice(0, 10));
+				setData(json.slice(0, 10)); // Fetch only the first 10 records for now
 			} catch (error) {
-				console.error("Fetch error:", error);
+				console.error("Error fetching data:", error);
 			}
-		}
+		};
 
 		fetchData();
 	}, []);
 
+	console.log("Rendering table with data:", data);
+
+	const table = useReactTable({
+		data,
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+	});
+
 	return (
 		<div>
-			<h1>Catch Records</h1>
-			<ul>
-				{records.map((record) => (
-					<li key={record._id}>
-						Date: {record.weekEndDate}, Area: {record.area}, Halibut:{" "}
-						{record.halibut} kg
-					</li>
-				))}
-			</ul>
+			<table>
+				<thead>
+					{table.getHeaderGroups().map((headerGroup) => (
+						<tr key={headerGroup.id}>
+							{headerGroup.headers.map((header) => (
+								<th key={header.id}>
+									{flexRender(
+										header.column.columnDef.header,
+										header.getContext()
+									)}
+								</th>
+							))}
+						</tr>
+					))}
+				</thead>
+				<tbody>
+					{table.getRowModel().rows.map((row) => (
+						<tr key={row.id}>
+							{row.getVisibleCells().map((cell) => (
+								<td key={cell.id}>
+									{flexRender(cell.column.columnDef.cell, cell.getContext())}
+								</td>
+							))}
+						</tr>
+					))}
+				</tbody>
+			</table>
 		</div>
 	);
 }
