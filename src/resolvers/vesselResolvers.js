2404dbdb0f0f98259740165ce7fccf24b354b73c
Rhys Smoker <r.smoker@gmail.com>
2023-12-05 10:22:46 -0800

    added alphabetical sorting to the getAllVessels resolver used by the vessel dropdown

--- src/resolvers/vesselResolvers.js
+++ src/resolvers/vesselResolvers.js
@@ -5,7 +5,7 @@ const vesselResolvers = {
 		// Fetch all vessels or a specific number if a limit is provided
 		getAllVessels: async (_, { limit }) => {
 			try {
-				const query = Vessel.find({});
+				const query = Vessel.find({}).sort({ name: 1 }); // Sort by name in ascending order
 				if (limit) {
 					query.limit(limit);
 				}
@@ -15,6 +15,7 @@ const vesselResolvers = {
 				throw new Error("Error fetching vessels");
 			}
 		},
+
 		// Fetch a single vessel by its ID
 		getVesselById: async (_, { id }) => {
 			try {
