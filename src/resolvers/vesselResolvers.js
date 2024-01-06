9dea9f20d0f7dd4613a6e9ea36bb6f5166e1923b
Rhys Smoker <r.smoker@gmail.com>
2024-01-06 11:49:58 -0800

    deleted duplicate getAllVessels query from vesselResovlers.js getAllVessels remains in carchRecordResolvers.js

--- src/resolvers/vesselResolvers.js
+++ src/resolvers/vesselResolvers.js
@@ -2,20 +2,6 @@ const Vessel = require("../models/VesselModel");
 
 const vesselResolvers = {
 	Query: {
-		// Fetch all vessels or a specific number if a limit is provided
-		getAllVessels: async (_, { limit }) => {
-			try {
-				const query = Vessel.find({}).sort({ name: 1 }); // Sort by name in ascending order
-				if (limit) {
-					query.limit(limit);
-				}
-				return await query.exec();
-			} catch (error) {
-				console.error("Error fetching vessels:", error);
-				throw new Error("Error fetching vessels");
-			}
-		},
-
 		// Fetch a single vessel by its ID
 		getVesselById: async (_, { id }) => {
 			try {
